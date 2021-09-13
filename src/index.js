329294cc014648655de8d339199f278b1ca354ee
C <earthhominid@gmail.com>
2021-09-13 16:41:35 -0300

    index logic added, rendering homepage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log("working")
+import renderHome from './home.js';
+
+// Function which adds home logic to logo
+const homeButton = () => {
+  const button = document.getElementById('logo');
+  logo.addEventListener('click', renderHomeButton);
+};
+
+const renderHomeButton = () => {
+  const main = document.querySelector('main');
+  main.textContent = '';
+  createNew();
+};
+
+const listenDepartmentClick = () => {
+  const main = document.querySelector('main');
+  const departmentLinks = document.querySelectorAll('.drop-box');
+
+  departmentLinks.forEach((icon) => {
+    icon.addEventListener('click', () => {
+      main.textContent = '';
+      if (icon.id === 'new-in') {
+        createNew();
+      } else if (icon.id === 'sofas') {
+        sofa();
+      } else if (icon.id === 'chairs') {
+        chair();
+      } else if (icon.id === 'favs') {
+        favorite();
+      }
+    });
+  });
+};
+renderHome();
+homeButton();
+listenDepartmentClick();
